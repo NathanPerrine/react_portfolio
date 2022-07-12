414c7940004092d90c477b9289f6f2a910dd8488
NathanPerrine <nathan.perrine@gmail.com>
2022-07-12 16:16:14 -0400

    favicon, title, import cleanup

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,7 @@ import PythonLogo from '../../assets/images/Python-logo.png'
 import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact, faPython } from '@fortawesome/free-brands-svg-icons'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 
 const About = () => {
